d2b9dd83bcd7f45a6a000e6721b001504a18e61e
tsaglisamuel <schaltzchagly@gmail.com>
2022-02-14 17:05:58 +0100

    initial commit

---
+++ assignment1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program program = new();
+            program.start();
+        }
+
+        void start()
+        {
+            Console.Write($"n = ");
+            int n = int.Parse(Console.ReadLine());
+            factorial(n);
+            Console.WriteLine("\n---------------------------------------");
+            recursiveFactorial(n);
+        }
+
+        //Assignment1A
+        int factorial(int n)
+        {
+            int total = 1;
+            Console.Write($"{n}! = ");
+            for (int i = 1; i <= n; i++)
+            {
+                total = i * total;
+                if (i < n)
+                {
+                    Console.Write($"{i} x ");
+                }
+                else
+                {
+                    Console.Write($"{i} = {total}");
+                }
+            }
+            return total;
+        }
+
+        //Assignment1B
+        int recursiveFactorial(int n)
+        {
+            //(n!) = n * (n-1)! //recursive
+            int f;
+            if (n > 1)
+            {
+               f = recursiveFactorial(n - 1) * n;
+                Console.WriteLine($"f({n}) = f({n-1}) x {n} = {f}");
+            }
+            else
+            {
+                Console.WriteLine($"f({n}) = {n}");
+                return 1;
+            }
+            return f;
+        }
+    }
+}
---
+++ assignment2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new();
+            program.start();
+        }
+        void start()
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            //fabonacci(n);
+            int result = fabonacciRecursive(n);
+            Console.WriteLine($"result: {result}");
+        }
+
+        //Assignment2A
+        void fabonacci(int n)
+        {
+            int f0 = 0;
+            int f1 = 1;
+
+            for(int i = 1; i < n; i++)
+            { 
+                Console.Write($"{f0}, ");
+                int sum = f0 + f1;
+                f0 = f1;
+                f1 = sum;
+            }
+        }
+
+
+        //Assignment2B
+        int fabonacciRecursive(int n)
+        {
+            int f;
+            if (n >= 2)
+            {
+                f = fabonacciRecursive(n - 1) + fabonacciRecursive(n - 2);
+            }
+            else
+            {
+                return n;
+            }
+            return f;
+
+
+            //if(n == 0)
+            //{
+            //    return 0;
+            //}
+            //else if (n == 1)
+            //{
+            //    return 1;
+            //}
+            //else
+            //{
+               
+            //    Console.WriteLine($"f({n}) = f({n - 1}) x {n} = {f}");
+            //}
+            //return f;
+           
+            
+        }
+    }
+}
